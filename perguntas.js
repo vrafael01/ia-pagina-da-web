a1deabe75a837a0949793be02b60f23135ccd056
vrafael01 <victor.pedroso.barbosa@escola.pr.gov.br>
2024-10-04 11:39:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Quem é o melhor jogador do MUNDO?',
+    'Cristiano Ronaldo  (CR7)'
+)
+
+criaCartao(
+    'Futebol',
+    'Melhor time de SP?',
+    'Sport Club Corinthians Paulista (SCCP) '
+)
+
+criaCartao(
+    'Futebol',
+    'Seleção com mais Copa do MUNDO?',
+    'Seleção Brasileira com 5 Copa do Mundo'
+)
+
+criaCartao(
+    'Futebol',
+    'Melhor goleiro de toda historia do futebol?',
+    'Manuel Neuer (Goleiro da seleção da Alemanha)  #01'
+)
